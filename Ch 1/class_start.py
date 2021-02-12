5259a91994cfda0ea271fc2329681802b52b5c42
smv1999 <vaidhyanathan.sm@gmail.com>
2021-02-12 09:17:17 +0530

    class methods and static methods

--- Ch 1/class_start.py
+++ Ch 1/class_start.py
@@ -3,27 +3,44 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
     # TODO: create a class method
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    @staticmethod
+    def getbooklist():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def setTitle(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
-
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 # TODO: access the class attribute
-
+print("Book Types : ", Book.getbooktypes())
 
 # TODO: Create some book instances
-b1 = Book("Title 1")
-b2 = Book("Title 2")
+b1 = Book("Title 1", "HARDCOVER")
+b2 = Book("Title 2", "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print(thebooks)
