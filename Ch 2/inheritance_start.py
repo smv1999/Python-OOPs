e5428c73d256a496b2941242561e998291c1b3b5
smv1999 <vaidhyanathan.sm@gmail.com>
2021-02-12 21:35:14 +0530

    Done with Inheritance

--- Ch 2/inheritance_start.py
+++ Ch 2/inheritance_start.py
@@ -1,29 +1,32 @@
-# Python Object Oriented Programming by Joe Marini course example
 # Understanding class inheritance
 
-
-class Book:
-    def __init__(self, title, author, pages, price):
+class Publication:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+class Periodical(Publication):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
 
 
-class Magazine:
+class Magazine(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, publisher, price, period)
 
 
-class Newspaper:
+class Newspaper(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, publisher, price, period)
 
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
