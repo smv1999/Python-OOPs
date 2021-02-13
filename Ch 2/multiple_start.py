3ff6e75b814f58580b1440d89aa551703ceb46c6
smv1999 <vaidhyanathan.sm@gmail.com>
2021-02-13 21:47:17 +0530

    Done with multiple inheritance

--- Ch 2/multiple_start.py
+++ Ch 2/multiple_start.py
@@ -6,17 +6,25 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
-
+        self.name = "Class A"
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = "Class B"
 
 
-class C(A, B):
+class C(B, A):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
 
 c = C()
+c.showprops()
+# method resolution order
+print(C.__mro__)
