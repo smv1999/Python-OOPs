a0d7719ef11d0265435f6eaea6cea7751aae6802
smv1999 <vaidhyanathan.sm@gmail.com>
2021-02-14 09:11:13 +0530

    Done with str and repr magic methods

--- Ch 3/magicstr_start.py
+++ Ch 3/magicstr_start.py
@@ -1,4 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
 # Using the __str__ and __repr__ magic methods
 
 
@@ -11,7 +10,13 @@ class Book:
 
     # TODO: use the __str__ method to return a string
 
+    def __str__(self):
+        return f"\"{self.title}\" by {self.author}, costs {self.price}"
+
     # TODO: use the __repr__ method to return an obj representation
+
+    def __repr__(self):
+        return f"title={self.title}, author={self.author}, price={self.price}"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -19,3 +24,8 @@ b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 print(b1)
 print(b2)
+
+print("\n")
+
+print(str(b1))
+print(repr(b2))
