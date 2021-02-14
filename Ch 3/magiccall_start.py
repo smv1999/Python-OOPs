af45237349765294aaca7a2227b8286f34072ebd
smv1999 <vaidhyanathan.sm@gmail.com>
2021-02-14 17:48:43 +0530

    Done with callable objects

--- Ch 3/magiccall_start.py
+++ Ch 3/magiccall_start.py
@@ -14,8 +14,15 @@ class Book:
 
     # TODO: the __call__ method can be used to call the object like a function
 
+    def __call__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 # TODO: call the object as if it were a function
+print(b1)
+b1("Wings of Fire", "APJ Abdul Kalam", 35.50)
+print(b1)
